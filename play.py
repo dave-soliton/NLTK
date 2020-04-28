18419e8f0f1beb7bcb0ac291fce5692797ab81d8
David Foster <33435292+dave-soliton@users.noreply.github.com>
2020-04-28 20:04:32 +0100

    dict

--- play.py
+++ play.py
@@ -10,3 +10,21 @@ def If_there(text):
         print('cat')
 
 If_there('the cat \n  sat house')
+
+import pandas as pd 
+  
+def Dicts(top):
+    dts = {'Sales':0,'Revenue':top}
+    return dts
+
+# initialize list of lists 
+data = [['tom', 10], ['nick', 15], ['juli', 14]] 
+  
+# Create the pandas DataFrame 
+df = pd.DataFrame(data, columns = ['Name', 'Age']) 
+df['DDts']=df['Age'].map(Dicts)
+
+# print dataframe. 
+print(df)
+
+print(df['DDts'][0]['Revenue'])
